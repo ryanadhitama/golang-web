87bb8e2db9ae1056256943a80d237d109352edfe
ryanvincere <ryanadhitama2@gmail.com>
2023-06-10 16:03:09 +0800

    handler

--- server_test.go
+++ server_test.go
@@ -1,13 +1,32 @@
 package golang_web
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 )
 
+// Test Server
 func TestServer(t *testing.T) {
 	server := http.Server {
 		Addr: "localhost:8080",
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Test Handler
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello World")
+	}
+	server := http.Server {
+		Addr: "localhost:8080",
+		Handler: handler,
 	}
 
 	err := server.ListenAndServe()
