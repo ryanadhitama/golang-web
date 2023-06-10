b1164021639ca693d3968e1d16216bb5e825f33c
ryanvincere <ryanadhitama2@gmail.com>
2023-06-10 19:54:28 +0800

    serve mux

--- server_test.go
+++ server_test.go
@@ -35,3 +35,27 @@ func TestHandler(t *testing.T) {
 		panic(err)
 	}
 }
+
+// ServeMux
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello World")
+	})
+
+	mux.HandleFunc("/ryan", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello Ryan")
+	})
+
+	server := http.Server {
+		Addr: "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
